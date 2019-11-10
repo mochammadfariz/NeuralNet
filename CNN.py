7f31a6aeaa5cefbb60a5c356ff5eb9f5b1620222
mochammadfariz <mochammadfariz11@gmail.com>
2019-11-10 21:27:52 +0700

    first commit

---
+++ CNN.py
@@ -0,0 +1,8 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten,Activation
+from keras.layers import Conv2D,MaxPooling2D
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
